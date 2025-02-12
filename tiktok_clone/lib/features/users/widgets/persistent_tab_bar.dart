4b5c65c030b6686fc5644a2870f5d979416b7f31
Soultree-Lovers <rkdtmdals999@naver.com>
2025-02-12 22:16:57 +0900

    [feat] User Profile 화면 UI 구현

---
+++ tiktok_clone/lib/features/users/widgets/persistent_tab_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class PersistentTabBar extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // TODO: implement build
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.symmetric(
+          horizontal: BorderSide(
+            color: Colors.grey.shade300,
+            width: 0.5,
+          ),
+        ),
+      ),
+      child: TabBar(
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorColor: Colors.black,
+        labelPadding: EdgeInsets.only(
+          top: 10,
+          bottom: 10,
+        ),
+        labelColor: Colors.black,
+        tabs: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(Icons.grid_4x4_rounded),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: FaIcon(FontAwesomeIcons.heart),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => 48; // 실제 탭바 높이와 같아야 함
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => 48;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    return false;
+  }
+
+}
